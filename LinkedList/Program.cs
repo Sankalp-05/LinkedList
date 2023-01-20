eab09dfb32e90a9e6588b83689ac84ae275becfd
Sankalp <sankalp.kulkarni05@gmail.com>
2023-01-20 22:08:45 +0530

    [Sankalp] - Created Simple Linked List

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Linked List");
+            Console.WriteLine("-------------LinkedList Operations-------------");
+            Console.WriteLine("1.Create simple Linked List of 56, 30 and 70 ");
+            Console.WriteLine("2. Create Linked List by Adding 30 and 56 to 70");
+            Console.WriteLine("------------------------------------------");
+            Console.Write("Enter the option : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            LinkedList linkedListStack = new LinkedList();
+            switch (num)
+            {
+                case 1:
+                    linkedListStack.Append(56);
+                    linkedListStack.Append(30);
+                    linkedListStack.Append(70);
+                    linkedListStack.Display();
+                    break;
+                default:
+                    Console.WriteLine("Please enter correct choice");
+                    break;
+            }
         }
     }
 }
